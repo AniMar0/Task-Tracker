759fc07a2dde1edf0e37978a5942ba622d1f8d5c
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-10-04 09:51:48 +0100

    GetTodos() function

--- tools/to-do.go
+++ tools/to-do.go
@@ -1,1 +1,49 @@
 package tools
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type Todo struct {
+	ID          int       `json:"id,omitempty"`
+	Description string    `json:"description"`
+	Status      string    `json:"status,omitempty"` //(todo, in-progress, done)
+	CreatedAt   time.Time `json:"createdAt,omitempty"`
+	UpdatedAt   time.Time `json:"updatedAt,omitempty"`
+}
+
+type Todos struct {
+	Todos []Todo `json:"todos"`
+}
+
+func MakeToDoFile() error {
+	file, err := os.Create("db.json")
+	if err != nil {
+		if os.IsExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+	return nil
+}
+
+func GetTodos() ([]Todo, error) {
+	var Todos []Todo
+	file, err := os.Open("db.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&Todos); err != nil {
+		if err.Error() == "EOF" {
+			return []Todo{}, nil
+		}
+		return nil, err
+	}
+	return Todos, nil
+}
